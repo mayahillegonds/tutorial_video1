4be9ead118148b88ebd59d4acd9a1ed263b8defa
mayahillegonds <maya.hillegonds1@gmail.com>
2024-12-27 22:31:13 +0000

    HW1 Commit

--- main.cpp
+++ main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() 
-{
-    
+{  
+
+  string name = "Maya";
+  char middle_initial = 'S';
+  int age = 21;
+  bool likes_mcdonalds = 1;
+  string favorite_game = "The Sims 4";
+  double favorite_number = 921.11;
+
+  // cout << name;
+  // cout << "\n";
+  // cout << "Maya\n";
+  // cout << "Sentence one," << " sentence two, " << "number one.\n";
+
+  cout << name << "\n";
+  cout << middle_initial << "\n";
+  cout << age << ", " << likes_mcdonalds << "\n";
+
+
 
   return 0;
 }
