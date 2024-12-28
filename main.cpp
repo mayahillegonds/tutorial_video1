ad457e945ba1346b893767e5f5deeb6e34971aa6
mayahillegonds <maya.hillegonds1@gmail.com>
2024-12-28 02:09:57 +0000

    Upload HW2

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cmath>
 using namespace std;
 
 int main() 
@@ -17,11 +18,46 @@ int main()
   // cout << "Maya\n";
   // cout << "Sentence one," << " sentence two, " << "number one.\n";
 
-  cout << name << "\n";
-  cout << middle_initial << "\n";
-  cout << age << ", " << likes_mcdonalds << "\n";
+  // cout << name << "\n";
+  // cout << middle_initial << "\n";
+  // cout << age << ", " << likes_mcdonalds << "\n";
+
+  string input;
+  cin >> input;
+  cout << input << " is what you input!\n";
+
+  /*
+  + addition
+  -
+  *
+  /
+  % modulo
+
+  10%2 = 0
+  11%2 = 1
+  */
+
+  int result;
+  cout << result << "\n";
+  5 + 5;
+  result = 5 + 5;
+  cout << result << "\n";
+  result = 10 / 2;
+  cout << result << "\n";
+
+  result = sqrt(4);
+  cout << result << "\n";
+  double base = 2.0;
+  double exponent = 3.0;
+  double result_power = pow(base, exponent);
+  cout << result_power << "\n";
 
 
+  result = 10 + 2;
+  result = result + 1;
+
+  result++; //+1 to result
+  result--; //-1 to result
 
   return 0;
 }
